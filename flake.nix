f7b726c696aa162af36b476056798c80d015a5bd
Tom McLaughlin <tom@codedown.io>
2025-11-16 04:22:12 -0800

    Update stack.yaml and add flake.nix + .envrc

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "WebSockets development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.haskell.compiler.ghc9122
+            pkgs.zlib
+            # pkgs.pkg-config
+          ];
+
+          shellHook = ''
+            echo "WebSockets development environment loaded"
+            echo "GHC version: $(ghc --version)"
+          '';
+        };
+      }
+    );
+}
